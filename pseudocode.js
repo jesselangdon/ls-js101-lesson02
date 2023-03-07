bf2d3c2cc334d6e3bbd01963317b426b3b59a1ee
jesselangdon <jesselangdon@gmail.com>
2023-03-07 22:49:32 +0000

    First commit of JS101, Lesson 2

---
+++ pseudocode.js
@@ -0,0 +1,64 @@
+/* a function that returns the sum of two numbers */
+//
+// START
+//   Ask user for first number
+//   SET number1 = user input
+//   ask user for the second number
+//   SET number2 = user input
+//   SET result = number1 + number2
+// PRINT result
+// END
+
+/* a function that takes an array of strings, and returns a string that is all 
+   those strings concatenated together */
+//
+// START
+//   SET stringArray = user input array
+//   SET returnArray
+//   SET iterator = 1
+//   WHILE iterator <= length of stringArray
+//    APPEND each string in stringArray to returnArray
+//    iterator = iterator + 1
+// RETURN returnArray
+// END
+
+/* a method that takes an array of integers, and returns a new array with every 
+   other element from the original array, starting with the first element */ 
+//
+// START
+//  SET userArray = user input array to be processed
+//  SET iterator = 1
+//  WHILE iterator <= lenght of userArray
+//    APPEND item in userArray
+//    iterator = itererator + 2
+//  RETURN userArray
+//  END
+
+/* a function that determines the index of the 3rd occurrence of a given 
+   character in a string. For instance, if the given character is 'x' and the 
+   string is 'axbxcdxex', the function should return 6 (the index of the 3rd 
+   'x'). If the given character does not occur at least 3 times, return null. */
+//
+// START
+//   SET userArray = user input array to be processed
+//   SET varCount = 0
+//   SET iterator = 1
+//   WHILE iterator < userArray length
+//   ???
+// END
+
+/* a function that takes two arrays of numbers and returns the result of merging
+   the arrays. The elements of the first array should become the elements at the
+   even indexes of the returned array, while the elements of the second array 
+   should become the elements at the odd indexes. */
+//
+// START
+// SET iterator= 0
+// SET mergeArray
+// CONCATENATE both arrays into a single array
+// FOR item in single array:
+//   append item to mergeArray
+//   iterator = iterator +3
+//   append item[iterator]
+// RETURN singleArray
+// END
